4a7efc1fd713ac783274d80eec706b1fc98ce4f3
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-03-09 12:12:22 +0000

    linked files to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-// import { Header } from "./layout";
+import { Header } from "./layout";
 import { Home, About } from "./pages";
 
 export const App = () => {
