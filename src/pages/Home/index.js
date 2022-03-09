17f752b663e776f8eaeb1f2b28fa7b7604492231
Noah Platton <noahplatton@gmail.com>
2022-03-09 12:54:09 +0000

    Set up page to interact with store

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import Form from '../../components/Form';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
+import { getUserInfo } from "../../actions";
+import { Form, Result } from "../../components";
 
 function Home() {
-    return (
-        <>
-            <Form />
-            
-        </>
-    )
+  const dispatch = useDispatch();
+
+  const result = useSelector((state) => state.result);
+  const username = useSelector((state) => state.username);
+  const loading = useSelector((state) => state.loading);
+  const error = useSelector((state) => state.error);
+
+  const search = (username) => dispatch(getUserInfo(username));
+
+  const renderResult = () =>
+    loading ? <p>Loading . . .</p> : <Result results={result} />;
+
+  return (
+    <>
+      <Form getUserInfo={search} />
+    </>
+  );
 }
 
 export default Home;
