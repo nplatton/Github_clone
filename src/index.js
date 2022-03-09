cb95be0f978e45fb33ba1c951e2736a40dcce417
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-03-09 10:46:20 +0000

    connected the index.js to index.html

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import store from "./store.js";
 import "./style/style.css";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root"));
